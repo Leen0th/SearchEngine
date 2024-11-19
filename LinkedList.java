330ab67d724a17a6f10fef7b9b20df73b057c60a
Reema Alkharaan <157988106+ReemaAbdulazizKh@users.noreply.github.com>
2024-11-19 14:29:21 +0300

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,14 +1,8 @@
 class LinkedList<T> {
-    
     private Node<T> head;
     private Node<T> current;
     private int size;
 
-    public int size() {
-        return size;
-    }
-
-    // Node inner class for the linked list
     private static class Node<T> {
         T data;
         Node<T> next;
@@ -19,45 +13,45 @@ class LinkedList<T> {
         }
     }
 
-    // Constructor
     public LinkedList() {
         head = null;
         current = null;
-        size=0;
+        size = 0;
     }
 
-    // Check if list is empty
+    // Returns the size of the linked list
+    public int size() {
+        return size;
+    }
+
+    // Checks if the list is empty
     public boolean empty() {
         return head == null;
     }
 
-    // Check if the node is at the end of the list 
+    // Checks if the current node is the last node in the list
     public boolean last() {
         return current != null && current.next == null;
     }
 
-    // Make the current node the first position of the list
+    // Sets the current node to the head of the list
     public void findfirst() {
         current = head;
     }
 
-    // Move to the next node of the current node in the list
+    // Moves the current node to the next node in the list
     public void findnext() {
-        if (current != null) { // Only move if current is not null
+        if (current != null) {
             current = current.next;
         }
     }
 
-    // Retrieve the data of the current node
+    // Retrieves the data of the current node
     public T retrieve() {
-        if (current == null) {
-            return null; // Return null if current is null, indicating the end or empty list
-        }
-    return current.data;
+        return current != null ? current.data : null;
     }
 
-
-    // Update the data of the current node 
+    // Updates the data of the current node
     public void update(T val) {
         if (current == null) {
             throw new IllegalStateException("Current is null, cannot update data.");
@@ -65,82 +59,73 @@ class LinkedList<T> {
         current.data = val;
     }
 
-    // Insert a node in the list (add)
+    // Inserts a new node with the given value after the current node
     public void insert(T val) {
         Node<T> newNode = new Node<>(val);
-        
         if (empty()) {
             head = newNode;
-            current = head; // Set current to the new head
+            current = head;
         } else {
-            newNode.next = current.next; // Link the new node to the next
-            current.next = newNode; // Insert the new node after current
-            current = newNode; // Move current to the new node
+            newNode.next = current.next;
+            current.next = newNode;
+            current = newNode;
         }
         size++;
     }
 
-    // Remove current node from the list
+    // Removes the current node from the list
     public void remove() {
         if (current == null) {
             throw new IllegalStateException("Current is null, cannot remove.");
         }
 
         if (current == head) {
-            head = head.next; // Update head if current is the first node
-            current = head; // Move current to the new head
+            head = head.next;
+            current = head;
         } else {
             Node<T> tmp = head;
-
             while (tmp != null && tmp.next != current) {
-                tmp = tmp.next; // Find the previous node
+                tmp = tmp.next;
             }
-
             if (tmp != null) {
-                tmp.next = current.next; // Bypass current node
+                tmp.next = current.next;
             }
-
-            current = (tmp != null) ? tmp.next : head; // Update current
+            current = (tmp != null) ? tmp.next : head;
         }
+        size--;
     }
 
-    // Search for an element by value and return index if found or -1 if not found
+    // Searches for an element by value and returns true if found
     public boolean search(T data) {
         Node<T> temp = head;
-
         while (temp != null) {
             if (temp.data.equals(data)) {
                 return true;
             }
             temp = temp.next;
         }
-
         return false; 
     }
 
-    // Display elements of the list
+    // Displays all elements of the list
     public void display() {
         Node<T> temp = head;
-
         while (temp != null) {
             System.out.print(temp.data + " -> ");
             temp = temp.next;
         }
-
         System.out.println("null");
     }
 
-    public String displaydocs() {
+    // Returns a string representation of the document list
+    public String displayDocs() {
         Node<T> temp = head;
-        String docs="";
-        while (temp.next!=null) {
-            docs+=temp.data + " -> ";
-           // System.out.print(temp.data + " -> ");
+        String docs = "";
+        while (temp != null) {
+            docs += temp.data + " -> ";
             temp = temp.next;
         }
-        docs+=temp.data ;
-
-        
+        docs += "null";
         return docs;
     }
 }
